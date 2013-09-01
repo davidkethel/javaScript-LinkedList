d7c649031ca21e35f8679f91e2b0097424079376
David Kethel <davidkethel@gmail.com>
2013-09-01 19:31:46 +1000

    Started adding some node tests

--- tests/spec/nodeSpec.js
+++ tests/spec/nodeSpec.js
@@ -1,7 +1,31 @@
 describe("Node", function(){
 
-it("should be able to do stuff", function(){
-	expect("dave").toEqual("dave");
+it("It should be able to hold a number", function(){
+	var myNode = new Node(3,null)
+	expect(myNode.getElement()).toEqual(3);
+});
+
+
+it("It should be able to hold a string", function(){
+	var myNode = new Node("Dave",null)
+	expect(myNode.getElement()).toEqual("Dave");
+});
+
+
+it("It should be able to hold a boolean", function(){
+	var myNode = new Node(true,null)
+	expect(myNode.getElement()).toEqual(true);
+});
+
+
+it("It should be able to hold a function", function(){
+	
+	var myFunc = function(){
+		return 1 + 1;
+	}
+
+	var myNode = new Node(myFunc,null)
+	expect(myNode.getElement()).toEqual(myFunc);
 });
 
 });
