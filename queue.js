0537cec8ed4be94d67a85983a862205264e94938
David Kethel <davidkethel@gmail.com>
2013-09-02 17:57:35 +1000

    more queue test

--- queue.js
+++ queue.js
@@ -1,19 +1,34 @@
 function Queue(){
 	
 	this.firstNode;
-
+	this.lastNode;
 
 	this.enQueue = function(v){
 
 		if(this.firstNode == undefined){
-			this.firstNode = new Node(v);
+			this.firstNode = new Node(v)
+			this.lastNode = this.firstNode;
 		}else{
-			this.firstNode.setNextNode(new Node(v));
+
+
+			var oldLastNode = this.lastNode;
+
+			oldLastNode.setNextNode(new Node(v));
+
+			this.lastNode = oldLastNode.getNextNode();
 		}
 	}
 
 
 	this.deQueue = function(){
-		return this.firstNode.getElement();
+
+		if(this.firstNode == undefined){
+			return null;
+		}
+		else{
+			var returnValue = this.firstNode.getElement();
+			this.firstNode = this.firstNode.getNextNode();
+			return returnValue;
+		}
 	}
 }
--- tests/spec/queueSpec.js
+++ tests/spec/queueSpec.js
@@ -1,5 +1,12 @@
 describe("Queue",function(){
 
+
+	it("Should be Able to handle an empty Queue",function(){
+		var myQueue = new Queue()	
+		expect(myQueue.deQueue()).toBeNull();
+	});
+
+
 	it("Should be Able to take one number",function(){
 		var myQueue = new Queue()
 		myQueue.enQueue(3);
@@ -44,9 +51,166 @@ describe("Queue",function(){
 	});
 
 
+	it("Should be Able to take two Strings",function(){
+		var myQueue = new Queue()
+		myQueue.enQueue("dave");
+		myQueue.enQueue("pil");
+		
+		expect(myQueue.deQueue()).toEqual("dave");
+	});
+
+	it("Should be Able to take 10 strings",function(){
+		var myQueue = new Queue()
+		myQueue.enQueue("dave1");
+		myQueue.enQueue("dave2");
+		myQueue.enQueue("dave3");
+		myQueue.enQueue("dave4");
+		myQueue.enQueue("dave5");
+		myQueue.enQueue("dave6");
+		myQueue.enQueue("dave7");
+		myQueue.enQueue("dave8");
+		myQueue.enQueue("dave9");
+		myQueue.enQueue("dave10");
+		expect(myQueue.deQueue()).toEqual("dave1");
+	});
+
+
+	it("Should be Able to take two booleans",function(){
+		var myQueue = new Queue()
+		myQueue.enQueue(false);
+		myQueue.enQueue(true);
+		
+		expect(myQueue.deQueue()).toEqual(false);
+	});
+
+	it("Should be Able to take 10 booleans",function(){
+		var myQueue = new Queue()
+		myQueue.enQueue(false);
+		myQueue.enQueue(true);
+		myQueue.enQueue(false);
+		myQueue.enQueue(true);
+		myQueue.enQueue(true);
+		myQueue.enQueue(false);
+		myQueue.enQueue(true);
+		myQueue.enQueue(false);
+		myQueue.enQueue(true);
+		myQueue.enQueue(true);
+		expect(myQueue.deQueue()).toEqual(false);
+	});
+
+	it("Should be Able to queue two numbers, and deQueue 2 numbers",function(){
+		var myQueue = new Queue()
+		myQueue.enQueue(3);
+		myQueue.enQueue(4);
+		
+		myQueue.deQueue();
+
+		expect(myQueue.deQueue()).toEqual(4);
+	});
+
+	
+
+	it("Should be Able to  queue 10 number and deQueue 10 numbers",function(){
+		var myQueue = new Queue()
+		myQueue.enQueue(1);
+		myQueue.enQueue(2);
+		myQueue.enQueue(3);
+		myQueue.enQueue(4);
+		myQueue.enQueue(5);
+		myQueue.enQueue(6);
+		myQueue.enQueue(7);
+		myQueue.enQueue(8);
+		myQueue.enQueue(9);
+		myQueue.enQueue(10);
+
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+
+
+		expect(myQueue.deQueue()).toEqual(10);
+	});
+
+it("Should be Able to queue two Strings and deQueue 2 strings",function(){
+		var myQueue = new Queue()
+		myQueue.enQueue("dave");
+		myQueue.enQueue("pil");
+
+		myQueue.deQueue();
+		
+		expect(myQueue.deQueue()).toEqual("pil");
+	});
 
 
 
+it("Should be Able to queue 10 strings and deQueue 10 string",function(){
+		var myQueue = new Queue()
+		myQueue.enQueue("dave1");
+		myQueue.enQueue("dave2");
+		myQueue.enQueue("dave3");
+		myQueue.enQueue("dave4");
+		myQueue.enQueue("dave5");
+		myQueue.enQueue("dave6");
+		myQueue.enQueue("dave7");
+		myQueue.enQueue("dave8");
+		myQueue.enQueue("dave9");
+		myQueue.enQueue("dave10");
+
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+
+
+		expect(myQueue.deQueue()).toEqual("dave10");
+	});
+
+it("Should be Able to queue two booleans and deQueue 2 booleans",function(){
+		var myQueue = new Queue()
+		myQueue.enQueue(false);
+		myQueue.enQueue(true);
+
+		myQueue.deQueue();
+		
+		expect(myQueue.deQueue()).toEqual(true);
+	});
+
+	it("Should be Able to queue 10 booleans and deQueue 10 booleans",function(){
+		var myQueue = new Queue()
+		myQueue.enQueue(false);
+		myQueue.enQueue(true);
+		myQueue.enQueue(false);
+		myQueue.enQueue(true);
+		myQueue.enQueue(true);
+		myQueue.enQueue(false);
+		myQueue.enQueue(true);
+		myQueue.enQueue(false);
+		myQueue.enQueue(true);
+		myQueue.enQueue(true);
+
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		myQueue.deQueue();
+		
+		expect(myQueue.deQueue()).toEqual(true);
+	});
 
 
 
