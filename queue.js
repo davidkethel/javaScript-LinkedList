25bfcb988258ab462383b4d2ec48bfda3783977b
davidkethel <davidkethel@gmail.com>
2013-09-03 09:19:02 +1000

    some queue formating

--- queue.js
+++ queue.js
@@ -3,32 +3,29 @@ function Queue(){
 	this.firstNode;
 	this.lastNode;
 
-	this.enQueue = function(v){
+    this.enQueue = function(v) {
 
-		if(this.firstNode == undefined){
-			this.firstNode = new Node(v)
-			this.lastNode = this.firstNode;
-		}else{
-
-
-			var oldLastNode = this.lastNode;
-
-			oldLastNode.setNextNode(new Node(v));
-
-			this.lastNode = oldLastNode.getNextNode();
-		}
-	}
+        if (this.firstNode == undefined) {
+            this.firstNode = new Node(v);
+            this.lastNode = this.firstNode;
+        } else {
 
 
-	this.deQueue = function(){
+            var oldLastNode = this.lastNode;
 
-		if(this.firstNode == undefined){
-			return null;
-		}
-		else{
-			var returnValue = this.firstNode.getElement();
-			this.firstNode = this.firstNode.getNextNode();
-			return returnValue;
-		}
-	}
+            oldLastNode.setNextNode(new Node(v));
+
+            this.lastNode = oldLastNode.getNextNode();
+        }
+    };
+    this.deQueue = function() {
+
+        if (this.firstNode == undefined) {
+            return null;
+        } else {
+            var returnValue = this.firstNode.getElement();
+            this.firstNode = this.firstNode.getNextNode();
+            return returnValue;
+        }
+    };
 }
