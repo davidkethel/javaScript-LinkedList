866a3fb3068616f271d1b4728f93e1b4033981d9
davidkethel <davidkethel@gmail.com>
2013-09-10 10:55:48 +1000

    Started list

---
+++ list.js
@@ -0,0 +1,29 @@
+function List() {
+
+
+    this.isEmpty = function() {
+        return true;
+    };
+
+    this.Head = function() {
+        return null;
+    };
+
+
+    this.Tail = function () {
+        return null;
+    };
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/spec/listSpec.js
+++ tests/spec/listSpec.js
@@ -1,9 +1,34 @@
 describe("list",function(){
 
-	it("should be able to do stuff", function(){
-		expect(1).toEqual(1);
+	it("should be able to do handle an empty List", function() {
+	    var myList = new List();
+	    expect(myList.isEmpty()).toEqual(true);
+
+	});
+
+    it("should be able to do handle an empty List when Head is called", function () {
+	    var myList = new List();
+	    expect(myList.Head()).toBeNull();
 	});
 
 
+	it("should be able to do handle an empty List when Tail is called", function () {
+	    var myList = new List();
+	    expect(myList.Tail()).toBeNull();
+	});
+
+
+
+    //Constructor
+    
+    //Is empty
+    
+    //Prepend
+    
+    //Append
+    
+    //Head
+    
+    //Tail
 
 })
